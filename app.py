d512fe7414477536394bd961eeb6f232355c7169
atenisoc <at_enisoc@yahoo.co.jp>
2025-07-07 15:16:35 +0900

    force: rebind to 0.0.0.0 and update port

--- app.py
+++ app.py
@@ -118,5 +118,5 @@ def upload_file():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Renderが提供するポート番号を取得
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
